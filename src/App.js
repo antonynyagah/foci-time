52533563f998019c04aa74f84edda0fb0a4d65f5
Antony Nyagah <antonynyagah@Antonys-MacBook-Pro.local>
2023-03-15 15:49:35 -0400

    added initial title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Title from './title';
 
 function App() {
   return (
-    <div>
-      test
+    <div className="App">
+      <Title />
     </div>
   );
 }
---
+++ src/title.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './title.css';
+
+function Title() {
+  return (
+    <div className="Title">
+    
+      <h1>Foci Time!</h1>
+            
+    </div>
+  )
+}
+
+export default Title
